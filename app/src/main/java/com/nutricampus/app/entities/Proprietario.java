93b8809d818080ed50a65391995097d3a9be00e2
Paulo Mateus <paulomatew@gmail.com>
2017-06-14 11:56:11 -0300

    Adição de Entidades

---
+++ app/src/main/java/com/nutricampus/app/entities/Proprietario.java
@@ -0,0 +1,10 @@
+package com.nutricampus.app.entities;
+
+/**
+ * Created by Mateus on 14/06/2017.
+ * For project NutriCampus.
+ * Contact: <paulomatew@gmail.com>
+ */
+
+public class Proprietario {
+}
