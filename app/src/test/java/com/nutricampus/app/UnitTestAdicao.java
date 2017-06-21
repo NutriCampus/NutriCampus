1dcc2db03801a3688ccce3cb853eba509948aa35
Paulo Mateus <paulomatew@gmail.com>
2017-06-21 00:45:14 -0300

    classestestes com falhas

--- app/src/test/java/com/nutricampus/app/UnitTestAdicao.java
+++ app/src/test/java/com/nutricampus/app/UnitTestAdicao.java
@@ -14,13 +14,11 @@ import static org.junit.Assert.*;
 public class UnitTestAdicao {
     @Test
     public void adicao_estaCorreto1() throws Exception {
-        Dieta p = new Dieta();
         assertEquals(4, 2 + 2);
     }
 
     @Test
     public void adicao_estaCorreto2() throws Exception {
-        Dieta p = new Dieta();
-        assertEquals(3, 2 + 1);
+        assertEquals(5, 2 + 1);
     }
 }
