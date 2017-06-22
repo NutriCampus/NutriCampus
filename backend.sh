9d45f56e56bef05080583b64704fd0dc3440c112
Paulo Mateus <paulomatew@gmail.com>
2017-06-21 23:55:01 -0300

    update backend.sh

--- backend.sh
+++ backend.sh
@@ -2,6 +2,7 @@
 # 1- esse bash altera o index.html do report para colocar o commit id e link do commit
 # 2- faz commit com uma deploy key para o projeto de reports no github
 
+echo "INICIOU ARQUIVO BACKEND.SH"
 username='NutriCampus'
 mainprojname='NutriCampus'
 reportprojname='NutriCampusUnitTestReport'
@@ -24,6 +25,7 @@ ismaster=`cd $pathprojeto && git rev-parse --abbrev-ref HEAD`
 #só faz o deployment do report se for o branch master que foi atualizado
 if [ "$ismaster" = "master" ];
 then
+	echo "is Master, then do scheme"
 	#regex
 	tofind1='<h1>Test Summary</h1>'
 	tofind2='<div id="footer">'
@@ -54,15 +56,16 @@ then
 	#fazer clone na raiz
 	cd $pathprojeto
 	
-	#apago conteúdo para alocar o novo conteúdo
+	#apago pasta se já existir
 	`rm -rf $pathprojetoreport`
+	#clono repositorio novamente e deleto tudo de dentro exceto pasta .git e README.md
 	`git clone $githubreportproject'.git' && cd $pathprojetoreport && find . \! -name '.git'  \! -name 'README.md' -delete`
 
-	#copiar arquivos
+	#copiar arquivos da pasta dos relatórios para pasta clonada
 	cp -a $pathreport. $pathprojetoreport
 
+	#acessar pasta do repositório report e commitar mudanças
 	cd $pathprojetoreport
-
-	#fazer commit com deploy key ou personal key
-	#git add -A && git commit -m "Main commit: https://github.com/ddefb/NutriCampus/commit/"$commitidLONG && git push -u origin master
+	git add -A && git commit -m "From commit: "$githubmainproject"/commit/"$commitidLONG && git push -u origin master
 fi
+echo "FINALIZOU ARQUIVO BACKEND.SH"
