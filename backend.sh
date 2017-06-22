817d1ac803cd33585de77e16ea140d12eb4cfaad
Paulo Mateus <paulomatew@gmail.com>
2017-06-22 14:59:38 -0300

    update travis.yml, update backend.sh, gradle e classe de testes

--- backend.sh
+++ backend.sh
@@ -5,6 +5,7 @@
 echo "INICIOU ARQUIVO BACKEND.SH"
 #pego hashpassword DO PARAMETRO DE ENTRADA para fazer commit (utilizando Environment Variables do travis)
 password=$1
+branch=$2
 username='NutriCampus'
 mainprojname='NutriCampus'
 reportprojname='NutriCampusUnitTestReport'
@@ -29,69 +30,74 @@ commitidLONG=`cd $pathprojeto && git rev-parse HEAD`
 echo "commit id LONG: "$commitidLONG
 commitidSHORT=`cd $pathprojeto && git rev-parse --short HEAD`
 echo "commit id SHORT: "$commitidSHORT
-ismaster=`cd $pathprojeto && git branch`
-echo "current branch (git branch): "$ismaster
+#ismaster=`cd $pathprojeto && git branch`
+#echo "current branch (git branch): "$ismaster
 
-#ifcond="* (detached from "$commitidSHORT") master"
-#if [ "$ismaster" = "$ifcond" ];
-#then
+echo "current branch (BRANCH ENVIRONMENT VARIABLE): "$branch
 
-#regex
-tofind1='<h1>Test Summary</h1>'
-tofind2='<div id="footer">'
+echo "COMPARE =="
+echo "::"$branch"::"
+echo "::master::"
 
-replacewith1='<h1>Test Summary</h1 <h4>main commit: <a href="'$githubmainproject'/commit/'$commitidLONG'" target="_blank">'$commitidSHORT'<a></h4>'
-replacewith2='<div id="footer"> Main project  @ <a href="'$githubmainproject'" target="_blank">'$mainprojname'</a><br /> Reports hosted @ <a href="'$githubreportproject'" target="_blank">'$reportprojname'</a>'
-#escrevo o conteudo no novo arquivo
-echo "-----ALTERANDO ARQUIVO index.html-----"
-cat $pathindexfile | while read line
-do
-	if [ "$line" = "$tofind1" ];
-	then
-		echo "ADICIONANDO LINK DO MAIN COMMIT AO index.html"
-		echo $replacewith1 >> $pathindexfiletemp
-	elif [ "$line" = "$tofind2" ];
-	then
-	echo "ADICIONANDO INFORMAÇÕES EXTRAS AO index.html"
-		echo $replacewith2 >> $pathindexfiletemp
-	else
-		echo $line >> $pathindexfiletemp
-	fi	
-done
-echo "-----ALTERAÇÕES CONCLUÍDAS NO ARQUIVO index.html-----"
+if [ "$branch" = "master" ];
+then
 
-#passo para o arquivo original
-indexhtml=`cat $pathindexfiletemp`
-echo $indexhtml > $pathindexfile
-echo "CONTEÚDO DO ARQUIVO TEMPORÁRIO PASSADO PARA O index.html"
+	#regex
+	tofind1='<h1>Test Summary</h1>'
+	tofind2='<div id="footer">'
 
-#excluo arquivo tmeporario
-rm $pathindexfiletemp
-echo "ARQUIVO TEMPORÁRIO DELETADO"
+	replacewith1='<h1>Test Summary</h1 <h4>main commit: <a href="'$githubmainproject'/commit/'$commitidLONG'" target="_blank">'$commitidSHORT'<a></h4>'
+	replacewith2='<div id="footer"> Main project  @ <a href="'$githubmainproject'" target="_blank">'$mainprojname'</a><br /> Reports hosted @ <a href="'$githubreportproject'" target="_blank">'$reportprojname'</a>'
+	#escrevo o conteudo no novo arquivo
+	echo "-----ALTERANDO ARQUIVO index.html-----"
+	cat $pathindexfile | while read line
+	do
+		if [ "$line" = "$tofind1" ];
+		then
+			echo "ADICIONANDO LINK DO MAIN COMMIT AO index.html"
+			echo $replacewith1 >> $pathindexfiletemp
+		elif [ "$line" = "$tofind2" ];
+		then
+		echo "ADICIONANDO INFORMAÇÕES EXTRAS AO index.html"
+			echo $replacewith2 >> $pathindexfiletemp
+		else
+			echo $line >> $pathindexfiletemp
+		fi	
+	done
+	echo "-----ALTERAÇÕES CONCLUÍDAS NO ARQUIVO index.html-----"
+
+	#passo para o arquivo original
+	indexhtml=`cat $pathindexfiletemp`
+	echo $indexhtml > $pathindexfile
+	echo "CONTEÚDO DO ARQUIVO TEMPORÁRIO PASSADO PARA O index.html"
+
+	#excluo arquivo tmeporario
+	rm $pathindexfiletemp
+	echo "ARQUIVO TEMPORÁRIO DELETADO"
 
 
-echo "-------------------------------------------------------------------"
-echo $indexhtml
-echo "-------------------------------------------------------------------"
-#fazer clone na raiz
-cd $pathprojeto
+	echo "-------------------------------------------------------------------"
+	echo $indexhtml
+	echo "-------------------------------------------------------------------"
+	#fazer clone na raiz
+	cd $pathprojeto
 
-#apago pasta se já existir
-rm -rf $pathprojetoreport
-echo "PASTA "$pathprojetoreport" APAGADA (SE EXISTIR)"
+	#apago pasta se já existir
+	rm -rf $pathprojetoreport
+	echo "PASTA "$pathprojetoreport" APAGADA (SE EXISTIR)"
 
-#clono repositorio novamente e deleto tudo de dentro exceto pasta .git e README.md
-git clone $githubreportproject'.git' && cd $pathprojetoreport && find . \! -name '.git'  \! -name 'README.md' -delete
-echo "PROJETO "$reportprojname" (GITHUB) CLONADO E CONTEÚDO APAGADO, EXCETO PASTA .git|README.md"
+	#clono repositorio novamente e deleto tudo de dentro exceto pasta .git e README.md
+	git clone $githubreportproject'.git' && cd $pathprojetoreport && find . \! -name '.git'  \! -name 'README.md' -delete
+	echo "PROJETO "$reportprojname" (GITHUB) CLONADO E CONTEÚDO APAGADO, EXCETO PASTA .git|README.md"
 
-#copiar arquivos da pasta dos relatórios para pasta clonada
-cp -a $pathreport. $pathprojetoreport
-echo "COPIADO CONTEÚDO DE "$pathreport" PARA "$pathprojetoreport
+	#copiar arquivos da pasta dos relatórios para pasta clonada
+	cp -a $pathreport. $pathprojetoreport
+	echo "COPIADO CONTEÚDO DE "$pathreport" PARA "$pathprojetoreport
 
-#acessar pasta do repositório report e commitar mudanças
-cd $pathprojetoreport
-git add -A && git commit -m "From commit: "$githubmainproject"/commit/"$commitidLONG
-git push https://$usernameofpersonalkey:$password@github.com/$username/$reportprojname.git && 	echo "COMMITADO COM SUCESSO!!"
+	#acessar pasta do repositório report e commitar mudanças
+	cd $pathprojetoreport
+	git add -A && git commit -m "From commit: "$githubmainproject"/commit/"$commitidLONG
+	git push https://$usernameofpersonalkey:$password@github.com/$username/$reportprojname.git && 	echo "COMMITADO COM SUCESSO!!"
 
-#fi
+fi
 echo "FINALIZOU ARQUIVO BACKEND.SH"
