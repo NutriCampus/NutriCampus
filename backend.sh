dbe00881ba3a4bb92181300d6ea04817d3630b6e
Paulo Mateus <paulomatew@gmail.com>
2017-06-23 13:38:04 -0300

    update backend.sh, update gradle: configurações para sonarqube

--- backend.sh
+++ backend.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 # 1- esse bash altera o index.html do report para colocar o commit id e link do commit
 # 2- faz commit com uma personal key para o projeto de reports no github
+echo "##################################################################"
+echo "##################################################################"
+echo "##################################################################"
+echo "##################                        ########################"
+echo "##################       BACKEND.SH       ########################"
+echo "##################                        ########################"
+echo "##################################################################"
+echo "##################################################################"
+echo "##################################################################"
 
-echo "INICIOU ARQUIVO BACKEND.SH"
 #pego hashpassword DO PARAMETRO DE ENTRADA para fazer commit (utilizando Environment Variables do travis)
 password=$1
 branch=$2
@@ -12,15 +20,16 @@ reportprojname='NutriCampusUnitTestReport'
 usernameofpersonalkey='tronipm'
 githubmainproject='https://github.com/'$username'/'$mainprojname
 githubreportproject='https://github.com/'$username'/'$reportprojname
-echo "github projeto: "$githubmainproject
-echo "github projeto report: "$githubreportproject
-
+echo ">>>>>>> github projeto: "$githubmainproject
+echo ">>>>>>> github projeto report: "$githubreportproject
+echo "##################################################################"
+echo "##################################################################"
 #pathprojeto='/home/matt/IDEs_workspaces/AndroidStudioProjects/'$mainprojname'/' #comentar essa linha quando rodar no travis
 pathprojeto='/home/travis/build/'$username'/'$mainprojname'/' #comentar essa linha quando rodar localmente
-echo "path home do projeto no travis: "$pathprojeto
+echo ">>>>>>> path home do projeto no travis: "$pathprojeto
 
 pathprojetoreport=$pathprojeto$reportprojname'/'
-echo "path home do projeto report no travis: "$pathprojetoreport
+echo ">>>>>>> path home do projeto report no travis: "$pathprojetoreport
 pathreport=$pathprojeto'app/build/reports/tests/testDebugUnitTest/'
 pathindexfile=$pathreport'index.html'
 pathindexfiletemp=$pathreport'temp'
@@ -30,13 +39,12 @@ lintresult=$pathlint'lint-results.xml'
 
 #pego os dados do commit principal
 commitidLONG=`cd $pathprojeto && git rev-parse HEAD`
-echo "commit id LONG: "$commitidLONG
+echo ">>>>>>> commit id LONG: "$commitidLONG
 commitidSHORT=`cd $pathprojeto && git rev-parse --short HEAD`
-echo "commit id SHORT: "$commitidSHORT
-#ismaster=`cd $pathprojeto && git branch`
-#echo "current branch (git branch): "$ismaster
-
-echo "current branch (BRANCH ENVIRONMENT VARIABLE): "$branch
+echo ">>>>>>> commit id SHORT: "$commitidSHORT
+echo ">>>>>>> current branch (BRANCH ENVIRONMENT VARIABLE): "$branch
+echo "##################################################################"
+echo "##################################################################"
 
 if [ "$branch" = "master" ];
 then
@@ -53,31 +61,31 @@ then
 	do
 		if [ "$line" = "$tofind1" ];
 		then
-			echo "ADICIONANDO LINK DO MAIN COMMIT AO index.html"
+			echo ">>>>>>> ADICIONANDO LINK DO MAIN COMMIT AO index.html"
 			echo $replacewith1 >> $pathindexfiletemp
 		elif [ "$line" = "$tofind2" ];
 		then
-		echo "ADICIONANDO INFORMAÇÕES EXTRAS AO index.html"
+		echo ">>>>>>> ADICIONANDO INFORMAÇÕES EXTRAS AO index.html"
 			echo $replacewith2 >> $pathindexfiletemp
 		else
 			echo $line >> $pathindexfiletemp
 		fi	
 	done
-	echo "-----ALTERAÇÕES CONCLUÍDAS NO ARQUIVO index.html-----"
+	echo ">>>>>>> -----ALTERAÇÕES CONCLUÍDAS NO ARQUIVO index.html-----"
 
 	#passo para o arquivo original
 	indexhtml=`cat $pathindexfiletemp`
 	echo $indexhtml > $pathindexfile
-	echo "CONTEÚDO DO ARQUIVO TEMPORÁRIO PASSADO PARA O index.html"
+	echo ">>>>>>> CONTEÚDO DO ARQUIVO TEMPORÁRIO PASSADO PARA O index.html"
 
 	#excluo arquivo tmeporario
 	rm $pathindexfiletemp
-	echo "ARQUIVO TEMPORÁRIO DELETADO"
-
-
-	echo "-------------------------------------------------------------------"
-	echo $indexhtml
-	echo "-------------------------------------------------------------------"
+	echo ">>>>>>> ARQUIVO TEMPORÁRIO DELETADO"
+	
+	echo "##################################################################"
+	echo "##################################################################"
+	#echo $indexhtml
+	#echo "-------------------------------------------------------------------"
 	#fazer clone na raiz
 	cd $pathprojeto
 
@@ -88,47 +96,46 @@ then
 	
 	git clone $githubreportproject'.git' && echo "PROJETO "$reportprojname" (GITHUB) CLONADO"
 	cd $pathprojetoreport
-	
-	echo "---------------------------------"
-	echo "IMPRIMINDO NOMES DE ARQUIVOS DO COMMIT ATUAL"
+
+	echo "##################################################################"
+	echo "##################################################################"
+	echo ">>>>>>> IMPRIMINDO NOMES DE ARQUIVOS DO COMMIT ATUAL NO "$reportprojname
 	ls -a
-	echo "---------------------------------"
+	echo ">>>>>>> ----------------------------------------------------------"
 	#find . \! -name '.git' \! -name 'README.md' -delete && 	echo "CONTEÚDO APAGADO, EXCETO PASTA .git|README.md"
 	
 	commitauxidlong=`cd $pathprojetoreport && git rev-parse HEAD`
-	echo $reportprojname" commit id LONG: "$commitidLONG
+	echo ">>>>>>> "$reportprojname" commit id LONG: "$commitidLONG
 	commitauxidshort=`cd $pathprojetoreport && git rev-parse --short HEAD`
-	echo $reportprojname" commit id SHORT: "$commitidSHORT
-	
-	#echo "---------------------------------"
-	#echo "IMPRIMINDO NOMES DE ARQUIVOS APÓS LIMPEZA NA PASTA"
-	#ls -a
-	#echo "---------------------------------"
+	echo ">>>>>>> "$reportprojname" commit id SHORT: "$commitidSHORT
 
-	echo "---------------------------------"
 	#tirar erro de branch com mudanças locais
-	echo "SETANDO CONFIGURAÇÕES DO GIT username E email"
+	echo ">>>>>>> SETANDO CONFIGURAÇÕES DO GIT username E email"
 	git config user.email "builds@travis-ci.org"
 	git config user.name "Travis CI"
 	git config push.default simple
 	#git pull
 	git checkout master
 	
-	echo "---------------------------------"
+	echo ">>>>>>> ----------------------------------------------------------"
 	mkdir 'lint'  && echo "PASTA 'lint' CRIADA"
 	#copiar arquivos da pasta dos relatórios para pasta clonada
-	cp -a $pathreport. $pathprojetoreport && echo "COPIADO CONTEÚDO DE "$pathreport" PARA "$pathprojetoreport
-	cp -a $lintindex $pathprojetoreport"lint/index.html" && echo "COPIADO "$lintindex" PARA "$pathprojetoreport"lint/index.html"
+	cp -a $pathreport. $pathprojetoreport && echo ">>>>>>> COPIADO CONTEÚDO DE "$pathreport" PARA "$pathprojetoreport
+	cp -a $lintindex $pathprojetoreport"lint/index.html" && echo ">>>>>>> COPIADO "$lintindex" PARA "$pathprojetoreport"lint/index.html"
 	#cp -a $lintresult $pathprojetoreport"lint/" && echo "COPIADO "$lintresult" PARA "$pathprojetoreport"lint/"
 	
-	echo "---------------------------------"
-	echo "IMPRIMINDO NOMES DE ARQUIVOS APÓS CONTEÚDO COPIADO"
+	echo ">>>>>>> ----------------------------------------------------------"
+	echo ">>>>>>> IMPRIMINDO NOMES DE ARQUIVOS APÓS CONTEÚDO COPIADO"
 	ls -a
-	echo "---------------------------------"
-	echo "IMPRIMINDO DIFERENÇAS: "
+	echo ">>>>>>> ----------------------------------------------------------"
+	echo ">>>>>>> IMPRIMINDO DIFERENÇAS: "
 	git commit
-	echo "---------------------------------"
-	#acessar pasta do repositório report e commitar mudanças
+	echo ">>>>>>> ----------------------------------------------------------"
+	
+	echo "##################################################################"
+	echo "##################################################################"
+	echo ">>>>>>> COMEÇANDO ENVIO DE DADOS PARA REPOSITÓRIO DE REPORT"
+	
 	git add -A && echo "GIT ADD SUCCESSFULLY"
 	git commit -m "From commit: "$githubmainproject"/commit/"$commitidLONG && echo "GIT COMMIT SUCCESSFULLY"
 	git push https://$usernameofpersonalkey:$password@github.com/$username/$reportprojname.git HEAD:master --force && echo "GIT PUSH SUCCESSFULLY"
