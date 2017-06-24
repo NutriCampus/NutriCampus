5553e55ea32c3975afcbe8be81f6d6a82c9b5465
ddefb <diego.defb@gmail.com>
2017-06-24 02:41:01 -0300

    Atualização do teste para animal.

--- app/src/test/java/com/nutricampus/app/TesteUnitarioAnimal.java
+++ app/src/test/java/com/nutricampus/app/TesteUnitarioAnimal.java
@@ -14,20 +14,11 @@ import static org.junit.Assert.assertNotNull;
  */
 public class TesteUnitarioAnimal {
     @Test
-    public void constructorAnimal5Arguments() throws Exception {
-        Animal p1 = new Animal("nome", "cpf", "nome", "cpf", "nome");
+    public void testarConstrutorSemArgumentos() throws Exception {
+        Animal p1 = new Animal();
         assertNotNull(p1);
 
-        p1 = new Animal(null, null, null, null, null);
-        assertNotNull(p1);
-    }
-
-    @Test
-    public void constructorAnima6Arguments() throws Exception {
-        Animal p1 = new Animal(0, "nome", "cpf", "nome", "cpf", "nome");
-        assertNotNull(p1);
-
-        p1 = new Animal(0, null, null, null, null, null);
+        p1 = new Animal();
         assertNotNull(p1);
     }
 }
