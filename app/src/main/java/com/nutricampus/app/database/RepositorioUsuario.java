5fef5be11dd302faef9523fd876d707c6eb8548e
fgpacheco <felipeguimaraes540@gmail.com>
2017-06-25 10:32:39 -0300

    [#146238341] Correção das classes de Banco de Dados

--- app/src/main/java/com/nutricampus/app/database/RepositorioUsuario.java
+++ app/src/main/java/com/nutricampus/app/database/RepositorioUsuario.java
@@ -1,134 +1,148 @@
 package com.nutricampus.app.database;
 
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
-import android.util.StringBuilderPrinter;
+import android.widget.Toast;
 
-import com.nutricampus.app.entities.Session;
+import com.nutricampus.app.activities.CadastrarUsuarioActivity;
+import com.nutricampus.app.activities.LoginActivity;
 import com.nutricampus.app.entities.Usuario;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by Diego Bezerra on 20/06/2017.
+ * Created by Felipe on 25/06/2017.
  * For project NutriCampus.
- * Contact: <diego.defb@gmail.com>
  */
 
 public class RepositorioUsuario {
 
+    private SQLiteManager gerenciador;
+    private SQLiteDatabase bancoDados;
+
+    public RepositorioUsuario(Context context) {
+        gerenciador = new SQLiteManager(context);
+    }
+
     public boolean inserirUsuario(Usuario usuario) {
+        bancoDados = gerenciador.getWritableDatabase();
 
         ContentValues dados = new ContentValues();
+        dados.put(SQLiteManager.USUARIO_COL_CRMV, usuario.getCrmv());
+        dados.put(SQLiteManager.USUARIO_COL_CPF, usuario.getCpf());
+        dados.put(SQLiteManager.USUARIO_COL_NOME, usuario.getNome());
+        dados.put(SQLiteManager.USUARIO_COL_EMAIL, usuario.getEmail());
+        dados.put(SQLiteManager.USUARIO_COL_SENHA, usuario.getSenha());
 
-        dados.put(SQLiteManager.usuario_col_crmv, usuario.getCRMV());
-        dados.put(SQLiteManager.usuario_col_cpf, usuario.getCpf());
-        dados.put(SQLiteManager.usuario_col_nome, usuario.getNome());
-        dados.put(SQLiteManager.usuario_col_email, usuario.getEmail());
-        dados.put(SQLiteManager.usuario_col_senha, usuario.getSenha());
 
-        long retorno;
-        try {
-            retorno = SQLiteManager.banco.insertOrThrow(SQLiteManager.tabelaUsuario, null, dados);
-        } catch (Exception e) {
-            retorno = 0;
-        }
+        long retorno = bancoDados.insert(SQLiteManager.TABELA_USUARIO, null, dados);
+        bancoDados.close();
 
-        if (retorno != 0 && retorno != -1) {
-            if (Session.DEBBUG)
-                Log.v(this.getClass().getName(), "inserirUsuario(): " +
-                        "CRMV=" + usuario.getCRMV() +
-                        " | Nome= " + usuario.getNome() +
-                        " | E-mail= " + usuario.getEmail());
-
-            System.out.println("Inserido com sucesso!");
-
-            return true;
-
-        } else {
-
+        if(retorno == -1) {
             return false;
+        } else {
+            return true;
         }
     }
 
     public Usuario buscarUsuario(String crmv) {
+        bancoDados = gerenciador.getReadableDatabase();
 
-        Cursor cursor = SQLiteManager.banco.query(
-                SQLiteManager.tabelaUsuario, null, SQLiteManager.usuario_col_crmv + " = ?",
-                new String[]{crmv}, null, null, null);
+        Cursor cursor = bancoDados.query(SQLiteManager.TABELA_USUARIO, new String[] {
+                        SQLiteManager.USUARIO_COL_ID,
+                        SQLiteManager.USUARIO_COL_CRMV,
+                        SQLiteManager.USUARIO_COL_NOME,
+                        SQLiteManager.USUARIO_COL_CPF,
+                        SQLiteManager.USUARIO_COL_EMAIL,
+                        SQLiteManager.USUARIO_COL_SENHA}, SQLiteManager.USUARIO_COL_CRMV + "=?",
+                new String[] { String.valueOf(crmv) }, null, null, null, null);
 
-        Usuario usuario = null;
-        if (cursor.moveToFirst()) {
-            usuario = new Usuario(
-                    cursor.getInt(cursor.getColumnIndex(SQLiteManager.usuario_col_id)),
-                    cursor.getString(cursor.getColumnIndex(SQLiteManager.usuario_col_crmv)),
-                    cursor.getString(cursor.getColumnIndex(SQLiteManager.usuario_col_cpf)),
-                    cursor.getString(cursor.getColumnIndex(SQLiteManager.usuario_col_nome)),
-                    cursor.getString(cursor.getColumnIndex(SQLiteManager.usuario_col_email)),
-                    cursor.getString(cursor.getColumnIndex(SQLiteManager.usuario_col_senha)));
-        }
-
-        if (Session.DEBBUG) {
-            Log.v(this.getClass().getName(), "buscarUsuario(): " +
-                    "ID=" + usuario.getId() +
-                    " | CRMV=" + usuario.getCRMV() +
-                    " | CPF=" + usuario.getCpf() +
-                    " | Nome= " + usuario.getNome() +
-                    " | E-mail= " + usuario.getEmail());
-
-            try {
-                cursor.close();
-
-            } catch (Exception e) {
-
-            }
+        if(cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            Usuario usuario = new Usuario(
+                    cursor.getInt(cursor.getColumnIndex(SQLiteManager.USUARIO_COL_ID)),
+                    cursor.getString(cursor.getColumnIndex(SQLiteManager.USUARIO_COL_CRMV)),
+                    cursor.getString(cursor.getColumnIndex(SQLiteManager.USUARIO_COL_CPF)),
+                    cursor.getString(cursor.getColumnIndex(SQLiteManager.USUARIO_COL_NOME)),
+                    cursor.getString(cursor.getColumnIndex(SQLiteManager.USUARIO_COL_EMAIL)),
+                    cursor.getString(cursor.getColumnIndex(SQLiteManager.USUARIO_COL_SENHA)));
 
             return usuario;
-
-        } else {
-            try {
-                cursor.close();
-
-            } catch (Exception e) {
-
-            }
-
-            return null;
-
         }
+        cursor.close();
+        return null;
     }
 
+    public ArrayList<Usuario> buscarTodosUsuarios() {
+
+        bancoDados = gerenciador.getReadableDatabase();
+
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        String getPessoas = "SELECT * FROM " + SQLiteManager.TABELA_USUARIO;
+
+        try {
+            Cursor c = bancoDados.rawQuery(getPessoas, null);
+
+            if (c.moveToFirst()) {
+                do {
+                    Usuario u = new Usuario();
+                    u.setId(c.getInt(c.getColumnIndex(SQLiteManager.USUARIO_COL_ID)));
+                    u.setCrmv(c.getString(c.getColumnIndex(SQLiteManager.USUARIO_COL_CRMV)));
+                    u.setCpf(c.getString(c.getColumnIndex(SQLiteManager.USUARIO_COL_CPF)));
+                    u.setNome(c.getString(c.getColumnIndex(SQLiteManager.USUARIO_COL_NOME)));
+                    u.setEmail(c.getString(c.getColumnIndex(SQLiteManager.USUARIO_COL_EMAIL)));
+                    u.setSenha(c.getString(c.getColumnIndex(SQLiteManager.USUARIO_COL_SENHA)));
+                    usuarios.add(u);
+                } while (c.moveToNext());
+                c.close();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            bancoDados.close();
+        }
+
+        return usuarios;
+    }
+
+
+
     public boolean atualizarUsuario(Usuario usuario) {
+        bancoDados = gerenciador.getWritableDatabase();
+
         ContentValues dados = new ContentValues();
 
-        dados.put(SQLiteManager.usuario_col_nome, usuario.getNome());
-        dados.put(SQLiteManager.usuario_col_email, usuario.getEmail());
-        dados.put(SQLiteManager.usuario_col_senha, usuario.getSenha());
+        dados.put(SQLiteManager.USUARIO_COL_NOME, usuario.getNome());
+        dados.put(SQLiteManager.USUARIO_COL_EMAIL, usuario.getEmail());
+        dados.put(SQLiteManager.USUARIO_COL_SENHA, usuario.getSenha());
 
-        long retorno = SQLiteManager.banco.update(SQLiteManager.tabelaUsuario,
-                dados, SQLiteManager.usuario_col_id, new String[]{String.valueOf(usuario.getId())});
+        int retorno = bancoDados.update(SQLiteManager.TABELA_USUARIO,
+                dados, SQLiteManager.USUARIO_COL_ID + " = ?",
+                new String[]{String.valueOf(usuario.getId())});
 
-        if (retorno != 0 && retorno != -1) {
-
-            if (Session.DEBBUG)
-                Log.v(this.getClass().getName(), "atualizarUsuario(): " +
-                        "ID=" + usuario.getId() +
-                        " | CRMV= " + usuario.getCRMV() +
-                        " | CPF= " + usuario.getCpf() +
-                        " | Nome= " + usuario.getNome() +
-                        " | E-mail= " + usuario.getEmail());
-
+        bancoDados.close();
+        if(retorno > 0) {
             return true;
-
         } else {
-
             return false;
         }
 
     }
 
     public void removerUsuario(Usuario usuario) {
-        SQLiteManager.banco.delete(SQLiteManager.tabelaUsuario, SQLiteManager.usuario_col_id + " = ?",
-                new String[]{String.valueOf(usuario.getId())});
+        bancoDados = gerenciador.getWritableDatabase();
+        bancoDados.delete(SQLiteManager.TABELA_USUARIO,
+                          SQLiteManager.USUARIO_COL_ID + " = ? ",
+                          new String[]{String.valueOf(usuario.getId())});
+
+        bancoDados.close();
     }
 
 }
--- app/src/main/java/com/nutricampus/app/database/SqliteManager.java
+++ app/src/main/java/com/nutricampus/app/database/SqliteManager.java
@@ -4,27 +4,26 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import java.util.ArrayList;
 
 /**
- * Created by Mateus on 14/06/2017.
+ * Created by Diego Bezerra on 20/06/2017.
  * For project NutriCampus.
- * Contact: <paulomatew@gmail.com>
+ * Contact: <diego.defb@gmail.com>
  */
-public class SQLiteManager {
+
+public class SQLiteManager extends SQLiteOpenHelper{
     /*
     http://randomkeygen.com/
     TODO trocar keys a cada versão do app
      */
 
     /* Nome do Banco de Dados */
-    private final String UNIQ_KEY = "5q7TA#Y";
-    private final String DATABASE_NAME = UNIQ_KEY + "NutriCampusDB";
-
-    /* Entidades do BD */
-    protected static final String tabelaUsuario = "Usuario";
+    private static final String NOME_BANCO = "NutriCampusBD";
+    private static final int VERSAO_BANCO = 1;
 
     /* Modo de acesso ao banco de dados
      *
@@ -35,52 +34,50 @@ public class SQLiteManager {
      * 2 - Modo escrita para todos (outras aplicações podem usar o banco). */
     private final int DATABASE_ACESS = 0;
 
+    protected static Context context;
+    public static final String TABELA_USUARIO = "usuario";
+    public static final String USUARIO_COL_ID = "_id";
+    public static final String USUARIO_COL_CRMV = "crmv";
+    public static final String USUARIO_COL_CPF = "cpf";
+    public static final String USUARIO_COL_NOME = "nome";
+    public static final String USUARIO_COL_EMAIL = "email";
+    public static final String USUARIO_COL_SENHA = "senha";
 
-    /* SQL de criação de tabelas. */
-
-    /* SQL de criação da tabela Usuario. */
-    protected static final String usuario_col_id = "ID", usuario_col_crmv = "CRMV",
-            usuario_col_cpf = "CPF", usuario_col_nome = "Nome", usuario_col_email = "Email",
-            usuario_col_senha = "Senha";
-
-    private final String SQL_CREATE_TABELA_USUARIO = "CREATE TABLE IF NOT EXISTS " + tabelaUsuario + "( " +
-            usuario_col_id + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-            usuario_col_crmv + " TEXT UNIQUE, " +
-            usuario_col_cpf + " TEXT, UNIQUE" +
-            usuario_col_nome + " TEXT NOT NULL, " +
-            usuario_col_email + " TEXT NOT NULL, " +
-            usuario_col_senha + " TEXT NOT NULL );";
-
-    /* Classe com métodos para executar os comandos SQL e manipular o banco de dados. */
-    protected static SQLiteDatabase banco;
-    protected static Context _context;
+    private final String SQL_CREATE_TABELA_USUARIO = "CREATE TABLE IF NOT EXISTS " + TABELA_USUARIO + "(" +
+            USUARIO_COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            USUARIO_COL_CRMV + " TEXT NOT NULL UNIQUE, " +
+            USUARIO_COL_CPF + " TEXT NOT NULL UNIQUE," +
+            USUARIO_COL_NOME + " TEXT NOT NULL, " +
+            USUARIO_COL_EMAIL + " TEXT NOT NULL, " +
+            USUARIO_COL_SENHA + " TEXT NOT NULL);";
 
     public SQLiteManager(Context context) {
-        this._context = context;
-        removerBdVersoesAnteriores();
-
-        this.banco = context.openOrCreateDatabase(DATABASE_NAME, DATABASE_ACESS, null);
-        this.banco.execSQL(SQL_CREATE_TABELA_USUARIO);
+        super(context, NOME_BANCO, null,VERSAO_BANCO);
+        this.context = context;
     }
 
-    /**
-     * NUNCA remover essa chamada. Isso fara com que seja apagado o bd anterior do usuário.
-     * Isso pq o usuário pode não ter a versão atual. Quando tiver, o bd anterior sera limpado.
-     */
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(SQL_CREATE_TABELA_USUARIO);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+
+    /*
     public void removerBdVersoesAnteriores() {
         final String bd1 = "nutriCampusDB";
         final String bd2 = "y=03p*48$4Zay;.9NutriCampusDB";
 
-        if (DATABASE_NAME.equals(bd1)) {
-            this._context.deleteDatabase(bd1);
-        } else if (DATABASE_NAME.equals(bd2)) {
-            this._context.deleteDatabase(bd2);
+        if (NOME_BANCO.equals(bd1)) {
+            this.context.deleteDatabase(bd1);
+        } else if (NOME_BANCO.equals(bd2)) {
+            this.context.deleteDatabase(bd2);
         }
     }
-
-    public void close() {
-        banco.close();
-        banco = null;
-    }
+    */
 
 }
