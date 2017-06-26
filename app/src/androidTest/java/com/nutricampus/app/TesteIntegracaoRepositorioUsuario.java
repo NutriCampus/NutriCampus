5794a949c5423d89d8e4d7e901c10b076be0983e
kellisonjk <kellisonjk@gmail.com>
2017-06-26 13:46:20 -0300

    Adição da classe TesteIntegracaoRepositorioUsuario

---
+++ app/src/androidTest/java/com/nutricampus/app/TesteIntegracaoRepositorioUsuario.java
@@ -0,0 +1,42 @@
+package com.nutricampus.app;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.nutricampus.app.activities.MainActivity;
+import com.nutricampus.app.database.RepositorioUsuario;
+import com.nutricampus.app.entities.Usuario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Diego Bezerra on 22/06/2017.
+ * For project NutriCampus.
+ * Contact: <diego.defb@gmail.com>
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class TesteIntegracaoRepositorioUsuario {
+
+    @Test
+    public void testBuscaDeUsuarioPeloCRMV(){
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        String crmv = "0123";
+        RepositorioUsuario repositorio = new RepositorioUsuario(appContext);
+
+        Usuario usuarioAdicionado = new Usuario(crmv,"000.000.000-00","usuario","usuario@email.com","123456");
+        repositorio.inserirUsuario(usuarioAdicionado);
+
+        Usuario usuarioEncontrado = repositorio.buscarUsuario(crmv);
+
+
+        assertEquals(usuarioAdicionado,usuarioEncontrado);
+    }
+}
