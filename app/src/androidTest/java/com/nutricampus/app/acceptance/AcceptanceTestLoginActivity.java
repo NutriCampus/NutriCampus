f9dae13fe7cdc5fefa6ff4f39f8d5a98e716130c
Paulo Mateus <paulomatew@gmail.com>
2017-06-30 16:32:00 -0300

    update classes teste

--- app/src/androidTest/java/com/nutricampus/app/acceptance/AcceptanceTestLoginActivity.java
+++ app/src/androidTest/java/com/nutricampus/app/acceptance/AcceptanceTestLoginActivity.java
@@ -25,6 +25,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.pressBack;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -32,6 +34,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.runner.lifecycle.Stage.RESUMED;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
@@ -113,10 +116,12 @@ public class AcceptanceTestLoginActivity {
     }
 
     @Test
-    public void attempToLoginSuccessfully() throws Exception {
+    /**
+     * Esse método precisa vir antes do attempToLoginSuccessfully, pq ele cria uma conta
+     */
+    public void attempToLoginCreatingAccount() throws Exception {
         doLogout();
 
-        //PRECISA CRIAR USUÀRIO PRIMEIRO
         Thread.sleep(1000);
         onView(withId(R.id.rlayout_faca_login)).perform(click());
         Thread.sleep(1000);
@@ -133,14 +138,16 @@ public class AcceptanceTestLoginActivity {
         closeKeyboard();
 
         onView(withId(R.id.btn_salvar_cadastro)).perform(click());
-        Thread.sleep(1000);//abrir dialog do cadastro confirmado
-        onView(withId(android.R.id.content)).perform(pressBack());//fecha dialog
-        //mActivityRule.getActivity().onBackPressed();//fecho dialog, automaticamente volta pra tela de login
-        Thread.sleep(1000);//fechar dialog
-        onView(withId(android.R.id.content)).perform(pressBack());//fecha activity
-        //mActivityRule.getActivity().onBackPressed();//fecho dialog, automaticamente volta pra tela de login
+        Thread.sleep(500);
+        onView(withText("Cadastro")).check(matches(isDisplayed()));
+    }
 
-        Thread.sleep(1000);//espera activity fechar
+    @Test
+    /**
+     * Usuário já foi cadastrado, então só tento fazer login
+     */
+    public void attempToLoginSuccessfully() throws Exception {
+        doLogout();
 
         //VOLTOU PARA TELA INICIAL
         onView(withId(R.id.input_usuario)).perform(typeText("63876813590"));
@@ -151,23 +158,7 @@ public class AcceptanceTestLoginActivity {
         onView(withId(R.id.btn_login)).perform(click());
         Thread.sleep(1000);//espera 1s p activity abrir
 
-        Activity actv = getActivityInstance();
-        Thread.sleep(1000);
-
-        if (!actv.getClass().getName().equals(MainActivity.class.getName())) {
-            assertTrue(false);
-        }
-
-        /*
-        * Não estou checando pelo toast pq o toast corresponde a LoginActivity (q após o login é
-        * finalizada), porém qnd o teste é feito, ele pega as views da MAINACTIVITY, assim sendo,
-        * o toast fazia parte da LOGINACTIVITY, ou seja, vai procurar um toast na view antiga,
-        * enquanto está sendo mostrada outrra, gerando assim SEMPRE erro. ISSO SÓ ACONTECE NO TRAVIS CI.
-        * */
-        //new ToastMatcher().isToastMessageDisplayedWithText("Bem-vindo ao NutriCampus");//funciona localmente
-        //onView(withText("Bem-vindo ao NutriCampus")).inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
-
-        Thread.sleep(3000);
+        assertTrue(mActivityRule.getActivity().isFinishing());
     }
 
     public void doLogout() throws Exception {
