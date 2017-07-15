433982f9e4fc4df0931d517b5f2639f0bb2e43b7
EduFelix <eduardofelix1221@gmail.com>
2017-07-14 21:24:03 -0300

    Commit teste para usúario já cadastrado.

---
+++ app/src/androidTest/java/com/nutricampus/app/activities/UsuarioJaCadastradoActivityTest2.java
@@ -0,0 +1,119 @@
+package com.nutricampus.app.activities;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.nutricampus.app.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class UsuarioJaCadastradoActivityTest2 {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void usuarioJaCadastradoActivityTest2() {
+        ViewInteraction relativeLayout = onView(
+                allOf(withId(R.id.rlayout_faca_login),
+                        withParent(withId(R.id.tela_loginactivity)),
+                        isDisplayed()));
+        relativeLayout.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.edtNome), isDisplayed()));
+        appCompatEditText.perform(replaceText("Eduardo"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.edtCpf), isDisplayed()));
+        appCompatEditText2.perform(click());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.edtCpf), isDisplayed()));
+        appCompatEditText3.perform(replaceText("09315359488"), closeSoftKeyboard());
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.edtRegistro), isDisplayed()));
+        appCompatEditText7.perform(replaceText("eeee"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText8 = onView(
+                allOf(withId(R.id.edtEmail), isDisplayed()));
+        appCompatEditText8.perform(replaceText("bbbbb@gmail.com"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText9 = onView(
+                allOf(withId(R.id.edtSenha), isDisplayed()));
+        appCompatEditText9.perform(replaceText("123456"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText10 = onView(
+                allOf(withId(R.id.edtSenha), withText("123456"), isDisplayed()));
+        appCompatEditText10.perform(pressImeActionButton());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_salvar_cadastro), withText("Salvar"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(android.R.id.message), withText("Usuário Eduardo cadastrado com sucesso !"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.scrollView),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Usuário Eduardo cadastrado com sucesso !")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(android.R.id.message), withText("Usuário Eduardo cadastrado com sucesso !"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.scrollView),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView2.check(matches(withText("Usuário Eduardo cadastrado com sucesso !")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
