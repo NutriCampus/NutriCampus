6a040e02cb8dfe5d6a27beaeb91b2055ba7039a9
ddefb <diego.defb@gmail.com>
2017-07-19 12:58:21 -0300

    Adição da classe utilitária de conversão

---
+++ app/src/main/java/com/nutricampus/app/utils/Conversor.java
@@ -0,0 +1,21 @@
+package com.nutricampus.app.utils;
+
+/**
+ * Created by Diego Bezerra on 15/06/17.
+ * For project NutriCampus.
+ * Contact: <diego.defb@gmail.com>
+ */
+
+public class Conversor {
+
+    public static int booleanToInt(boolean bol) {
+        return ( bol ? 1 : 0);
+    }
+
+    public static boolean intToBoolean(int i) {
+        return ( i == 1 );
+    }
+
+    public static boolean StringToBoolean(String s) { return (s.equals("true"));}
+
+}
