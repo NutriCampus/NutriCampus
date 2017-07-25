b9a3adaa4c2c03bb3acb91b57e2303651c1e1e94
Paulo Mateus <paulomatew@gmail.com>
2017-07-25 16:11:09 -0300

    [#147429353] TAs edição produção de leite

--- app/src/androidTest/java/com/nutricampus/app/acceptance/ProducaoDeLeiteEdicaoAcceptanceTest.java
+++ app/src/androidTest/java/com/nutricampus/app/acceptance/ProducaoDeLeiteEdicaoAcceptanceTest.java
@@ -20,8 +20,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
@@ -33,12 +36,14 @@ import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.runner.lifecycle.Stage.RESUMED;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -62,15 +67,15 @@ public class ProducaoDeLeiteEdicaoAcceptanceTest {
     }
 
     @Test
-    public void producaoDeLeiteEdicao() {
+    public void producaoDeLeiteEdicaoTA3() {
 
         Calendar calendarJunho = Calendar.getInstance();
-        calendarJunho.set(2017, Calendar.JUNE, 1);
+        calendarJunho.set(2017, Calendar.JUNE, 2);
         Calendar calendarJulho = Calendar.getInstance();
-        calendarJulho.set(2017, Calendar.JULY, 1);
+        calendarJulho.set(2017, Calendar.JULY, 2);
         RepositorioProducaoDeLeite repositorioProducaoDeLeite = new RepositorioProducaoDeLeite(InstrumentationRegistry.getTargetContext());
-        ProducaoDeLeite producaoDeLeite1 = new ProducaoDeLeite(calendarJunho, 1, 99, 99, 99, 99, 99);
-        ProducaoDeLeite producaoDeLeite2 = new ProducaoDeLeite(calendarJulho, 1, 88, 88, 88, 88, 88);
+        ProducaoDeLeite producaoDeLeite1 = new ProducaoDeLeite(1, calendarJunho, 1, 99, 99, 99, 99, 99);
+        ProducaoDeLeite producaoDeLeite2 = new ProducaoDeLeite(2, calendarJulho, 1, 88, 88, 88, 88, 88);
         repositorioProducaoDeLeite.inserirProducaoDeLeite(producaoDeLeite1);
         repositorioProducaoDeLeite.inserirProducaoDeLeite(producaoDeLeite2);
 
@@ -141,8 +146,12 @@ public class ProducaoDeLeiteEdicaoAcceptanceTest {
                 allOf(withId(android.R.id.text1), withText("Julho"), isDisplayed()));
         appCompatTextView5.perform(click());
 
+        ViewInteraction appCompatButton10 = onView(
+                allOf(withId(R.id.btn_filtrar_producao), isDisplayed()));
+        appCompatButton10.perform(click());
 
-        onView(withText("01/07/2017"))//Peso 99,00 kg
+
+        onView(withText("02/07/2017"))//Peso 99,00 kg
                 .perform(longClick());
 
         ViewInteraction appCompatTextView2 = onView(
@@ -175,6 +184,240 @@ public class ProducaoDeLeiteEdicaoAcceptanceTest {
 
         try {
             new ToastMatcher().isToastMessageDisplayedWithText("Registro atualizado com sucesso");
+            Thread.sleep(3500);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        repositorioProducaoDeLeite.removerProducaoDeLeite(producaoDeLeite1);
+        repositorioProducaoDeLeite.removerProducaoDeLeite(producaoDeLeite2);
+    }
+
+    @Test
+    public void producaoDeLeiteEdicaoTA2() {
+
+        Calendar calendarJunho = Calendar.getInstance();
+        calendarJunho.set(2017, Calendar.JUNE, 1);
+        Calendar calendarJulho = Calendar.getInstance();
+        calendarJulho.set(2017, Calendar.JULY, 1);
+        RepositorioProducaoDeLeite repositorioProducaoDeLeite = new RepositorioProducaoDeLeite(InstrumentationRegistry.getTargetContext());
+        ProducaoDeLeite producaoDeLeite1 = new ProducaoDeLeite(1, calendarJunho, 1, 99, 99, 99, 99, 99);
+        ProducaoDeLeite producaoDeLeite2 = new ProducaoDeLeite(2, calendarJulho, 1, 88, 88, 88, 88, 88);
+        repositorioProducaoDeLeite.inserirProducaoDeLeite(producaoDeLeite1);
+        repositorioProducaoDeLeite.inserirProducaoDeLeite(producaoDeLeite2);
+
+        if (getActivityInstance() instanceof MainActivity) {
+            //dummy if
+            try {
+                Thread.sleep(4500);//toast de boas vindas
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+
+            ViewInteraction appCompatEditText = onView(
+                    allOf(withId(R.id.input_usuario), isDisplayed()));
+            appCompatEditText.perform(click());
+            ViewInteraction appCompatEditText2 = onView(
+                    allOf(withId(R.id.input_usuario), isDisplayed()));
+            appCompatEditText2.perform(replaceText("admin"), closeSoftKeyboard());
+
+            ViewInteraction appCompatEditText3 = onView(
+                    allOf(withId(R.id.input_senha), isDisplayed()));
+            appCompatEditText3.perform(replaceText("admin"), closeSoftKeyboard());
+
+            ViewInteraction appCompatButton = onView(
+                    allOf(withId(R.id.btn_login), withText("Entrar"), isDisplayed()));
+            appCompatButton.perform(click());
+
+        }
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Abrir"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.material_drawer_recycler_view),
+                        withParent(allOf(withId(R.id.material_drawer_slider_layout),
+                                withParent(withId(R.id.material_drawer_layout)))),
+                        isDisplayed()));
+        recyclerView.perform(actionOnItemAtPosition(5, click()));
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withText("animalAdmin UM"))
+                .perform(longClick());
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(android.R.id.title), withText("Ver produção de leite"), isDisplayed()));
+        appCompatTextView.perform(click());
+
+        ViewInteraction appCompatSpinner4 = onView(
+                allOf(withId(R.id.spinner_meses), isDisplayed()));
+        appCompatSpinner4.perform(click());
+
+        ViewInteraction appCompatButton10 = onView(
+                allOf(withId(R.id.btn_filtrar_producao), isDisplayed()));
+        appCompatButton10.perform(click());
+
+        ViewInteraction appCompatTextView5 = onView(
+                allOf(withId(android.R.id.text1), withText("Julho"), isDisplayed()));
+        appCompatTextView5.perform(click());
+
+
+        onView(withText("01/07/2017"))//Peso 99,00 kg
+                .perform(longClick());
+
+        ViewInteraction appCompatTextView2 = onView(
+                allOf(withId(android.R.id.title), withText("Editar"), isDisplayed()));
+        appCompatTextView2.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.btn_salvar_producao), withText("Atualizar")));
+        appCompatButton4.perform(scrollTo(), click());
+
+        try {
+            new ToastMatcher().isToastMessageDisplayedWithText("Registro atualizado com sucesso");
+            Thread.sleep(3500);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        repositorioProducaoDeLeite.removerProducaoDeLeite(producaoDeLeite1);
+        repositorioProducaoDeLeite.removerProducaoDeLeite(producaoDeLeite2);
+    }
+
+    @Test
+    public void producaoDeLeiteEdicaoTA1() {
+
+        Calendar calendarJunho = Calendar.getInstance();
+        calendarJunho.set(2017, Calendar.JUNE, 1);
+        Calendar calendarJulho = Calendar.getInstance();
+        calendarJulho.set(2017, Calendar.JULY, 1);
+        RepositorioProducaoDeLeite repositorioProducaoDeLeite = new RepositorioProducaoDeLeite(InstrumentationRegistry.getTargetContext());
+        ProducaoDeLeite producaoDeLeite1 = new ProducaoDeLeite(1, calendarJunho, 1, 99, 99, 99, 99, 99);
+        ProducaoDeLeite producaoDeLeite2 = new ProducaoDeLeite(2, calendarJulho, 1, 88, 88, 88, 88, 88);
+        repositorioProducaoDeLeite.inserirProducaoDeLeite(producaoDeLeite1);
+        repositorioProducaoDeLeite.inserirProducaoDeLeite(producaoDeLeite2);
+
+        if (getActivityInstance() instanceof MainActivity) {
+            //dummy if
+            try {
+                Thread.sleep(4500);//toast de boas vindas
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+
+            ViewInteraction appCompatEditText = onView(
+                    allOf(withId(R.id.input_usuario), isDisplayed()));
+            appCompatEditText.perform(click());
+            ViewInteraction appCompatEditText2 = onView(
+                    allOf(withId(R.id.input_usuario), isDisplayed()));
+            appCompatEditText2.perform(replaceText("admin"), closeSoftKeyboard());
+
+            ViewInteraction appCompatEditText3 = onView(
+                    allOf(withId(R.id.input_senha), isDisplayed()));
+            appCompatEditText3.perform(replaceText("admin"), closeSoftKeyboard());
+
+            ViewInteraction appCompatButton = onView(
+                    allOf(withId(R.id.btn_login), withText("Entrar"), isDisplayed()));
+            appCompatButton.perform(click());
+
+        }
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Abrir"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.material_drawer_recycler_view),
+                        withParent(allOf(withId(R.id.material_drawer_slider_layout),
+                                withParent(withId(R.id.material_drawer_layout)))),
+                        isDisplayed()));
+        recyclerView.perform(actionOnItemAtPosition(5, click()));
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withText("animalAdmin UM"))
+                .perform(longClick());
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(android.R.id.title), withText("Ver produção de leite"), isDisplayed()));
+        appCompatTextView.perform(click());
+
+        ViewInteraction appCompatSpinner4 = onView(
+                allOf(withId(R.id.spinner_meses), isDisplayed()));
+        appCompatSpinner4.perform(click());
+
+        ViewInteraction appCompatTextView5 = onView(
+                allOf(withId(android.R.id.text1), withText("Julho"), isDisplayed()));
+        appCompatTextView5.perform(click());
+        
+        ViewInteraction appCompatButton10 = onView(
+                allOf(withId(R.id.btn_filtrar_producao), isDisplayed()));
+        appCompatButton10.perform(click());
+
+
+        onView(withText("01/07/2017"))//Peso 99,00 kg
+                .perform(longClick());
+
+        ViewInteraction appCompatTextView2 = onView(
+                allOf(withId(android.R.id.title), withText("Editar"), isDisplayed()));
+        appCompatTextView2.perform(click());
+
+        ViewInteraction appCompatEditText21 = onView(
+                allOf(withId(R.id.input_quantidade_leite), isDisplayed()));
+        appCompatEditText21.perform(replaceText(""), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText22 = onView(
+                allOf(withId(R.id.input_lactose), isDisplayed()));
+        appCompatEditText22.perform(replaceText(""), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText23 = onView(
+                allOf(withId(R.id.input_gordura), isDisplayed()));
+        appCompatEditText23.perform(replaceText(""), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText25 = onView(
+                allOf(withId(R.id.input_proteina_bruta), isDisplayed()));
+        appCompatEditText25.perform(replaceText(""), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText26 = onView(
+                allOf(withId(R.id.input_proteina_verdadeira), isDisplayed()));
+        appCompatEditText26.perform(replaceText(""), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.btn_salvar_producao), withText("Atualizar")));
+        appCompatButton4.perform(scrollTo(), click());
+
+        try {
+            new ToastMatcher().isToastMessageDisplayedWithText("Campos inválidos");
             Thread.sleep(3500);
         } catch (Exception e) {
             e.printStackTrace();
